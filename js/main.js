656588bd6ac55e14c2d386552b2812ac1646d380
gooner16 <lukec978@gmail.com>
2016-08-03 01:04:55 -0400

    Populate drop down

--- js/main.js
+++ js/main.js
@@ -1,28 +1,14 @@
-function loadBox () {
-    $(document).ready(function() {
-        $.ajax({
-            type: 'GET',
-            url: 'http://www.fueleconomy.gov/ws/rest/vehicle/menu/year',
-            dataType: 'xml',
-            success: function(xml) {
-                $(xml).find('menuItem').each(function() {
-                    $('#year')
-                        .append($('<option />')
-                        .text($(this)
-                        .find('text').text()));
-                });
-            }
-        });
-    });
+var selYear, selMake, selModel, selOption;
 
+function getData (dataLabel, dest, store) {
     $(document).ready(function() {
         $.ajax({
             type: 'GET',
-            url: 'http://www.fueleconomy.gov/ws/rest/vehicle/menu/make?year=2012',
+            url: 'http://www.fueleconomy.gov/ws/rest/vehicle/menu/'+dataLabel+'',
             dataType: 'xml',
             success: function(xml) {
                 $(xml).find('menuItem').each(function() {
-                    $('#selectYear')
+                    $('#'+dest+'')
                         .append($('<option />')
                         .text($(this)
                         .find('text').text()));
@@ -30,6 +16,69 @@ function loadBox () {
             }
         });
     });
+}
+///reduce below functions
+function getSelect (sel) {
+    
+    if (sel.id == "year"){
+        selYear = sel.options[sel.selectedIndex].text;
+        console.log(selYear)
+        $(document).ready(function() {
+            $.ajax({
+                type: 'GET',
+                url: 'http://www.fueleconomy.gov/ws/rest/vehicle/menu/make?year='+selYear+'',
+                dataType: 'xml',
+                success: function(xml) {
+                    $(xml).find('menuItem').each(function() {
+                        $('#make')
+                            .append($('<option />')
+                            .text($(this)
+                            .find('text').text()));
+                    });
+                }
+            });
+        });
+    } else if (sel.id == "make"){
+        selMake = sel.options[sel.selectedIndex].text;
+        console.log(selMake)
+        //////////////////////////
+        $(document).ready(function() {
+            $.ajax({
+                type: 'GET',
+                url: 'http://www.fueleconomy.gov/ws/rest/vehicle/menu/make?year='+selYear+'',
+                dataType: 'xml',
+                success: function(xml) {
+                    $(xml).find('menuItem').each(function() {
+                        $('#make')
+                            .append($('<option />')
+                            .text($(this)
+                            .find('text').text()));
+                    });
+                }
+            });
+        });
+    }else if (sel.id == "model"){
+        selModel = sel.options[sel.selectedIndex].text;
+        console.log(selModel)
+        $(document).ready(function() {
+            $.ajax({
+                type: 'GET',
+                url: 'http://www.fueleconomy.gov/ws/rest/vehicle/menu/make?year='+selYear+'',
+                dataType: 'xml',
+                success: function(xml) {
+                    $(xml).find('menuItem').each(function() {
+                        $('#make')
+                            .append($('<option />')
+                            .text($(this)
+                            .find('text').text()));
+                    });
+                }
+            });
+        });
+    }else{
+        selOption = sel.options[sel.selectedIndex].text;
+        console.log(selOption)
+    }
 }
     
 
