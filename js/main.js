082209c7a27c8242d765bce0f8dcc0bbc8c7988c
Patrick Harris <patricklewery.harris@mail.utoronto.ca>
2016-09-11 22:25:49 -0400

    Linting

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var vehicleInfo = new Array(6);
 //clears options from dropdown box
 function removeOptions(selectbox)
 {
-    
+
     if (selectbox.id == "year") {
         amount = 3;
     }else if (selectbox.id == "make"){
@@ -15,8 +15,8 @@ function removeOptions(selectbox)
         amount = 0;
     }
 
-    for (var j = 1; j <= amount; j++) 
-    {    
+    for (var j = 1; j <= amount; j++)
+    {
         if (j == 1) {
             selectbox = document.getElementById("options");
         }else if (j == 2){
@@ -24,12 +24,12 @@ function removeOptions(selectbox)
         }else if (j == 3){
             selectbox = document.getElementById("make");
         }
-        
-        for(var i = selectbox.options.length - 1 ; i > 0 ; i--) 
+
+        for(var i = selectbox.options.length - 1 ; i > 0 ; i--)
         {
             selectbox.remove(i);
-        };
-    };
+        }
+    }
 }
 
 //grab data for:
@@ -49,7 +49,7 @@ function getEmissions(id) {
             val: null,
         };
         vehicleInfo.push(tempInfo);
-    };
+    }
 
     $(document).ready(function() {
         $.ajax({
@@ -59,11 +59,11 @@ function getEmissions(id) {
             success: function (xml) {
 
                 for (var i = 0; i < vehicleInfo.length; i++) {
-                    $(xml).find(vehicleInfo[i].label).each(function () {
+                    $(xml).find(vehicleInfo[i].label).each(function () { /* Function within a loop?? */
                         vehicleInfo[i].val = $(this).val();
-                        console.log(vehicleInfo[i].val)
+                        console.log(vehicleInfo[i].val);
                     });
-                }               
+                }
             }
         });
     });
@@ -86,13 +86,13 @@ function getData (dataLabel, dest, io) {
                             .find('text').text()));
                     });
                 //for getting the vehicle id
-                } else if (io == 0){
+              } else if (io === 0){
                     $(xml).find('menuItem').each(function() {
                         if (selOption == $(this).find("text").text()) {
                             vehicleid = $(this).find("value").text();
                             console.log(vehicleid);
                             getEmissions(vehicleid);
-                        } 
+                        }
                     });
                 }
             }
@@ -102,30 +102,29 @@ function getData (dataLabel, dest, io) {
 
 //organize which data is loaded and cleared when fields are changed
 function getSelect (sel) {
-    
+
     if (sel.id == "year"){
         selYear = sel.options[sel.selectedIndex].text;
-        console.log(selYear)
+        console.log(selYear);
         removeOptions(document.getElementById("year"));
-        getData('menu/make?year='+selYear+'', 'make', 1)
+        getData('menu/make?year='+selYear+'', 'make', 1);
 
     } else if (sel.id == "make"){
         selMake = sel.options[sel.selectedIndex].text;
-        console.log(selMake)
+        console.log(selMake);
         removeOptions(document.getElementById("make"));
-        getData('menu/model?year='+selYear+'&make='+selMake+'', 'model', 1)
-        
+        getData('menu/model?year='+selYear+'&make='+selMake+'', 'model', 1);
+
     }else if (sel.id == "model"){
         selModel = sel.options[sel.selectedIndex].text;
-        console.log(selModel)
+        console.log(selModel);
         removeOptions(document.getElementById("model"));
-        getData('menu/options?year='+selYear+'&make='+selMake+'&model='+selModel+'', 'options', 1)
-        
+        getData('menu/options?year='+selYear+'&make='+selMake+'&model='+selModel+'', 'options', 1);
+
     }else{
         selOption = sel.options[sel.selectedIndex].text;
-        console.log(selOption)
+        console.log(selOption);
         removeOptions(document.getElementById("options"));
-        getData('menu/options?year='+selYear+'&make='+selMake+'&model='+selModel+'', 'options', 0)
+        getData('menu/options?year='+selYear+'&make='+selMake+'&model='+selModel+'', 'options', 0);
     }
 }
-
